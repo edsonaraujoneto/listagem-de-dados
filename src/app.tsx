810ef9d0a715829a40610919978abbb3e6763ab9
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-02-21 15:14:29 -0300

    build: adicionar react-dom-router

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,46 @@ import { Tabs } from './componentes/tabs.tsx'
 import { Button } from './componentes/ui/button.tsx'
 import { Input,Control } from './componentes/ui/input.tsx'
 import { Table, TableHeader,TableRow,TableHead, TableBody,TableCell } from './componentes/ui/table'
-import { Pagination } from './componentes/pagination.tsx'
+import { Pagination } from './componentes/pagination'
+import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+
+  const [searchParams] = useSearchParams()
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+
+  const { data: tagsResponse , isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10`)
+      const data = await response.json()
+
+      console.log(data)
+      return data
+    },
+  })
+
+  if (isLoading ) {
+    return null
+  }
 
   return (
     <div className="py-10 space-y-8">
@@ -50,19 +87,19 @@ export function App() {
           </TableHeader>
 
           <TableBody>
-            {Array.from( { length: 10}).map((value,index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className='flex flex-col gap-0.5'>
-                      <span className='font-medium'>React</span>
-                      <span className='text-xs text-zinc-500'>45a28d07-2b06-4f87-bd4c-7d7fe149821a</span>
+                      <span className='font-medium'>{tag.title}</span>
+                      <span className='text-xs text-zinc-500'>{tag.id}</span>
                     </div>
                   </TableCell>
     
                   <TableCell className='text-zinc-500'>
-                    13 video(s)
+                    {tag.amountOfVideos} video(s)
                   </TableCell>
     
                   <TableCell className='text-right'>
@@ -78,7 +115,8 @@ export function App() {
 
         </Table>
         
-        {/* <Pagination/> */}
+        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page}/> }
+
       </main>
     </div>
   )
